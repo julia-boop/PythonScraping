ebad7faa79c46338e3ab8c530b664ee8a60acdc2
julia-boop <juliacordero@Julias-MacBook-Air.local>
2025-02-05 15:59:49 -0300

    chromium path

--- GetFile.py
+++ GetFile.py
@@ -27,6 +27,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/chromium"
 
 driver = webdriver.Chrome(options=chrome_options)
 
@@ -37,7 +38,7 @@ chrome_options.add_experimental_option("prefs", {
     "safebrowsing.enabled": True               
 })
 
-service = Service()
+service = Service("/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 def get_latest_file(directory):
